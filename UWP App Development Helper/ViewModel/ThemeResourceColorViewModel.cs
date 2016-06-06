a47a2a35f6f55c080ddd42c88771d2e7ee7c0577
Tom Lambert <tom.lambert.13@live.com>
2016-06-06 15:50:51 +0200

    Added ThemeResource Loadinganimation

--- UWP App Development Helper/ViewModel/ThemeResourceColorViewModel.cs
+++ UWP App Development Helper/ViewModel/ThemeResourceColorViewModel.cs
@@ -5,9 +5,11 @@ using System.Windows.Input;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using GalaSoft.MvvmLight.Command;
+using PostSharp.Patterns.Model;
 
 namespace Koopakiller.Apps.UwpAppDevelopmentHelper.ViewModel
 {
+    [NotifyPropertyChanged]
     public class ThemeResourceColorViewModel : HeaderViewModelBase
     {
         public ThemeResourceColorViewModel()
@@ -15,18 +17,42 @@ namespace Koopakiller.Apps.UwpAppDevelopmentHelper.ViewModel
             this.LoadThemeResourcesCommand = new RelayCommand(async () => await this.LoadThemeResourcesAsync());
         }
 
+        private bool _areThemeResourcesLoaded;
+        private bool _isLoading;
+
         public ObservableCollection<ColorViewModel> ThemeResources { get; } = new ObservableCollection<ColorViewModel>();
 
         public ICommand LoadThemeResourcesCommand { get; }
 
+        public bool IsLoading
+        {
+            get { return this._isLoading; }
+            set
+            {
+                this._isLoading = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
         private async Task LoadThemeResourcesAsync()
         {
+            if (this._areThemeResourcesLoaded)
+            {
+                return;
+            }
+
+            this.IsLoading = true;
+
+            this.ThemeResources.Clear();
             var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Resources/ThemeResources.txt"));
             var lines = await FileIO.ReadLinesAsync(file, UnicodeEncoding.Utf8);
             foreach (var line in lines)
             {
                 this.ThemeResources.Add(new ColorViewModel(line));
             }
+            this._areThemeResourcesLoaded = true;
+
+            this.IsLoading = false;
         }
     }
 
