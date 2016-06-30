3ade0a1cad173246c23f19a309480a6ab0e0f92a
Tom Lambert <tom.lambert.13@live.com>
2016-06-30 23:45:38 +0200

    Dirty hack for preventing lack in Pivot

---
+++ UWP App Development Helper/Controls/MyPivot.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Koopakiller.Apps.UwpAppDevelopmentHelper.Controls
+{
+    public class MyPivot : Pivot
+    {
+        public MyPivot()
+        {
+            this.SelectionChanged += this.OnSelectionChanged;
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var width = this.Width;
+            this.Width = double.IsInfinity(width) || double.IsNaN(width) ? 100 : width + 1;
+            this.UpdateLayout();
+            this.Width = width;
+        }
+    }
+}
